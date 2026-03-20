1a4e66c2685220645044c645a8a1cc1dd1fa3a9c
James Jensen <jamesarthurjensen@gmail.com>
2026-03-20 12:48:42 +0800

    fixup: Indentation in blockquotes

--- quartz/plugins/transformers/indentedMathsFix.ts
+++ quartz/plugins/transformers/indentedMathsFix.ts
@@ -4,7 +4,7 @@ export const IndentedMathsFix: QuartzTransformerPlugin = () => {
 	return {
 		name: "Indented maths fix",
 		textTransform(_ctx, src) {
-			return src.replace(/(\t?)(\S) +\$\$/g, "$1$2\n$1\t$$$$")
+			return src.replace(/((?:> )?\t*)([^ >]+) +\$\$/g, "$1$2\n$1\t$$$$")
 		},
 	}
 }
